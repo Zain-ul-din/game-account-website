4c01af0163fb96536322b40d9e9725bd1f8b33d6
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-26 21:00:10 +0500

    style: prose responsiveness

--- src/app/company/privacy/page.tsx
+++ src/app/company/privacy/page.tsx
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs'
 export default function Page() {
   return (
     <div className="w-full py-16 px-4 bg-white">
-      <div className="max-w-screen-xl mx-auto flex flex-col prose">
+      <div className="max-w-screen-xl mx-auto flex flex-col prose prose-sm prose-blue md:prose-lg">
         <Markdown>
           {readFileSync(`${process.cwd()}/src/docs/privacy.md`, 'utf-8')}
         </Markdown>
