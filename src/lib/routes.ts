f0d7fbbe64c129df2f25d16618d0e27135f730ea
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-27 17:57:28 +0500

    feat: added AppAds.txt link

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,5 +3,5 @@ export const routes = {
   'Games': '/games',
   'Contact': '/contact',
   'About Us': '/about',
-  'AppAds.txt': 'https://www.google.com'
+  'AppAds.txt': 'https://x248f18ed.app-ads-txt.com'
 }
