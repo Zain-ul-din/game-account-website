e38a792bb08b14658226fb6e2131a5e30468b155
Zain_ul_din <78583049+Zain-ul-din@users.noreply.github.com>
2024-05-24 05:09:46 +0000

    add about us

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,32 @@ export default function About() {
       <div className="max-w-screen-xl mx-auto flex flex-col gap-12">
         <div>
           <h1 className="text-3xl font-bold">About Us</h1>
+          <p className="mt-6">
+            Welcome to Hixbox, a dynamic sub-unit within
+            <a href="https://www.gamedistrict.co/" className="px-1 underline" target="_blank">Game District</a>, dedicated to pushing the boundaries of interactive entertainment. Our team of passionate game developers, designers, and artists is committed to creating immersive open-world action games that captivate players and redefine gaming experiences.
+          </p>
+          <h1 className="text-xl font-bold mt-6">
+            Our Journey
+          </h1>
+          <p className="mt-4">
+            Our journey began with a passion for action games, where our innovative approach and dedication to quality quickly earned us success and recognition in the gaming industry. Building on this foundation, we have expanded our horizons to include the development of hyper-casual games, bringing our creative flair and technical expertise to a wider audience
+          </p>
+          <h1 className="text-xl font-bold mt-6">
+            Our Vision
+          </h1>
+          <p className="mt-4">
+            At Hixbox, our vision is to create games that are not only fun and engaging but also rich in narrative and interactivity. We strive to craft expansive open-world environments where players can explore, adventure, and become part of compelling stories. Our hyper-casual games, on the other hand, are designed for quick, enjoyable experiences that are perfect for gaming on the go.
+          </p>
+          <h1 className="text-xl font-bold mt-6">
+            Join Us on Our Adventure
+          </h1>
+          <p className="mt-4">
+            As we continue to grow and evolve, we invite you to join us on our adventure. Whether you're a seasoned gamer or new to the gaming world, there's something for everyone at Hixbox. Stay tuned for our latest releases and updates as we continue to innovate and deliver top-notch gaming experiences.
+
+            Thank you for being a part of our journey. Together, we'll explore new worlds and create unforgettable gaming moments.
+
+            Welcome to Hixbox – where imagination meets reality.
+          </p>
         </div>
       </div>
     </div>
